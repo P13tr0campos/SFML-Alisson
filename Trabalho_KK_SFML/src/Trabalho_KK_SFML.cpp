c0b52f33884f553e1afbeada78c37a5c173c3d12
campo <campo@DESKTOP-2HFL9DA>
2024-08-29 20:07:19 -0300

    comit teste

---
+++ Trabalho_KK_SFML/src/Trabalho_KK_SFML.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Trabalho_KK_SFML.cpp
+// Author      : PietroCampos
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
