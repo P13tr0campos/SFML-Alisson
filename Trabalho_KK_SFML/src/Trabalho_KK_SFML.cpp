45bb12ddf57a2c0edc4b6c16d4c06ba8052a07f3
Pietro Campos de Sousa <136477657+P13tr0campos@users.noreply.github.com>
2024-08-30 18:02:26 -0300

    configurações sfml

--- Trabalho_KK_SFML/src/Trabalho_KK_SFML.cpp
+++ Trabalho_KK_SFML/src/Trabalho_KK_SFML.cpp
@@ -7,9 +7,33 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include <cstdio>
+#include <cstdlib>
+#include <math.hpp>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
-}
+ #include<SFML/Graphics.hpp>
+
+//Código principal.
+ int main()
+ {
+ sf::RenderWindow window(
+ sf::VideoMode(160,192),
+ "HelloWorld");
+ sf ::CircleShape shape (6);
+
+ // Abertura da janela.
+
+ while(window.isOpen())
+ {
+ sf::Event event;
+ while(
+ window.pollEvent(event))
+ if(event.type==
+ sf::Event::Closed)
+ window.close();
+ window.clear();
+ window.draw(shape);
+ window.display();
+ }
+ return 0;
+ }
