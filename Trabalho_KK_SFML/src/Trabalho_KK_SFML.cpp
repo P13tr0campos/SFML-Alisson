3f48155598af6d11de5f4adfef4fb2ddfdd70616
vitorialobato <136477023+vitorialobato@users.noreply.github.com>
2024-09-01 18:18:30 -0300

    Update Trabalho_KK_SFML.cpp

--- Trabalho_KK_SFML/src/Trabalho_KK_SFML.cpp
+++ Trabalho_KK_SFML/src/Trabalho_KK_SFML.cpp
@@ -2,16 +2,52 @@
 #include <cstdio>
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
-#include<SFML/Window.hpp>
-#include<SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <vector>
 #include "includes/math.hpp"
 
-int main()
-{
-        sf::RenderWindow window{ sf::VideoMode(986,771), "The game!" };
-        sf::Texture t;
-        t.loadFromFile("Background/Fundo.PNG");
-        sf::Sprite s(t);
+
+int main(){
+	// Criação da janela
+	sf::RenderWindow window{ sf::VideoMode(986,771), "King Kong" };
+
+//------------------------------------FUNDO-------------------------------------------------------------//
+
+	// Carregar textura do Fundo a partir de um arquivo
+        sf::Texture texturaFundo;
+        if(!texturaFundo.loadFromFile("Fundo.png")){
+        	 std::cout << "Erro ao carregar a textura do arquivo" << std::endl;
+        	 return -1;
+        }
+
+        // Criar um sprite do Fundo para desenhar a textura
+        sf::Sprite SpriteFundo(texturaFundo);
+        SpriteFundo.setPosition(0,0);
+
+//------------------------------------BOMBA-------------------------------------------------------------//
+
+        sf::Texture texturaBomba;
+           if (!texturaBomba.loadFromFile("bomb1.png"))
+           {
+               std::cerr << "Erro ao carregar a textura do arquivo" << std::endl;
+               return -1;
+           }
+           else{
+        	   sf::Sprite spriteBomba;
+        	   spriteBomba.setTexture(texturaBomba);
+
+
+           }
+
+           // Criar um sprite e associá-lo à textura
+           sf::Sprite spriteBomba;
+           spriteBomba.setTexture(texturaBomba);
+
+           spriteBomba.setPosition(180, 180);
+
+
+//Loop da janela
         while (window.isOpen())
         {
                 sf::Event windowEvent;
@@ -20,8 +56,14 @@ int main()
                         if (windowEvent.type == sf::Event::Closed)
                                 window.close();
                 }
+
+
+
                 window.clear(sf::Color::White);
-                window.draw(s);
+
+//Desenhar
+                window.draw(SpriteFundo);
+                 window.draw(spriteBomba);
                 window.display();
         }
         return 0;
