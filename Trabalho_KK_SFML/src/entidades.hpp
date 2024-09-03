4086069f05c0f4b00f5aa270bbdb3385be6c65cf
sophiaferreiraa <137004738+sophiaferreiraa@users.noreply.github.com>
2024-09-03 00:09:09 -0300

    Update entidades.hpp

--- Trabalho_KK_SFML/src/entidades.hpp
+++ Trabalho_KK_SFML/src/entidades.hpp
@@ -4,10 +4,10 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-#include <vector>
 #include <SFML/Graphics/Rect.hpp>
 #include "includes/math.hpp"
 
@@ -20,114 +20,118 @@ public:
     Fundo() {
         if (!texturaFundo.loadFromFile("Fundo.png")) {
             std::cout << "Erro ao carregar a textura do arquivo" << std::endl;
-
         }
         spriteFundo.setTexture(texturaFundo);
         spriteFundo.setPosition(0, 0);
     }
-    void draw(sf::RenderWindow& window) {
-           window.draw(spriteFundo);
-      }
 };
-//-----------------------------------------------BOMBA---------------------------------------------
+
+//-----------------------------------------------BOMBA---------------------------------------------//
 class Bomba {
 public:
     sf::Texture texturaBomba[3];
     sf::Sprite spriteBomba;
     int textura_corrente = 0;
-    int f = 0;
-    // Define a velocidade de movimento
-      float speed = 200.0f;
-      bool movingRight = true;
-      sf::Clock movementClock;
+    int contador = 0; // Contador para troca de animação
+    float velocidade = 200.0f; // Velocidade de movimento da bomba
+    bool MovimentoDireita = true; // Direção do movimento da bomba
+    sf::Clock TempoMovimento; // Relógio para calcular o tempo de movimento
+    sf::Clock TempoAnimacao;// Relógio para calcular o tempo de animação
 
+
+      // Limites da faixa onde a bomba pode se mover
+          float DistanciaEsquerda = 150.f;   // Distância da borda esquerda da janela
+          float DistanciaDireita = 830.f; // Distância da borda direita da janela (986 - largura da bomba)
+
+
+    //Construtor
     Bomba() {
         texturaBomba[0].loadFromFile("bombC.png", sf::IntRect(0, 0, 32, 96));
         texturaBomba[1].loadFromFile("bombC.png", sf::IntRect(32, 0, 64, 96));
         texturaBomba[2].loadFromFile("bombC.png", sf::IntRect(64, 0, 96, 96));
         spriteBomba.setTexture(texturaBomba[textura_corrente]);
-
         spriteBomba.setPosition(130, 130);
     }
 
-
+    // Atualiza a animação da bomba
     void update() {
-            f++;
-            if (f >= 10) {
-                f = 0;
-                // Muda para a próxima textura da animação
-                textura_corrente = (textura_corrente + 1) % 3;
-                spriteBomba.setTexture(texturaBomba[textura_corrente]);
+            if (TempoAnimacao.getElapsedTime().asMilliseconds() >= 100) { // Troca a textura a cada 100ms
+                contador++;
+                if (contador >= 10) { // Após 10 iterações, reinicia o contador
+                    contador = 0;
+                    textura_corrente = (textura_corrente + 1) % 3;
+                    spriteBomba.setTexture(texturaBomba[textura_corrente]);
+                }
+                TempoAnimacao.restart(); // Reinicia o relógio de animação
             }
         }
 
+    void movimentacao(sf::RenderWindow& window) {
+            sf::Time Tempo = TempoMovimento.restart();
 
-int movimentacao(sf::RenderWindow& window){
-    sf::Time deltaTime = movementClock.restart();
-
-    if (movingRight) {
-        spriteBomba.move(speed * deltaTime.asSeconds(), 0);
-        if (spriteBomba.getPosition().x + spriteBomba.getGlobalBounds().width >= window.getSize().x) {
-            movingRight = false;
+            // Move a bomba para a direita ou esquerda e verifica colisão com as bordas da faixa verde
+            if (MovimentoDireita) {
+                spriteBomba.move(velocidade * Tempo.asSeconds(), 0); // Move a bomba para a direita
+                if (spriteBomba.getPosition().x + spriteBomba.getGlobalBounds().width >= DistanciaDireita) {
+                	MovimentoDireita = false; // Inverte a direção ao atingir o limite direito
+                }
+            } else {
+                spriteBomba.move(-velocidade * Tempo.asSeconds(), 0); // Move a bomba para a esquerda
+                if (spriteBomba.getPosition().x <= DistanciaEsquerda) {
+                	MovimentoDireita = true; // Inverte a direção ao atingir o limite esquerdo
+                }
+            }
         }
-    } else {
-        spriteBomba.move(-speed * deltaTime.asSeconds(), 0);
-        if (spriteBomba.getPosition().x <= 0) {
-            movingRight = true;
+
+      void colisaoBomba(sf::RenderWindow& window) {
+            movimentacao(window); // Atualiza a movimentação da bomba
+            update(); // Atualiza a animação da bomba
         }
-    }
-
-    return 0;
-}
-
-    void draw(sf::RenderWindow& window) {
-        window.draw(spriteBomba);
-    }
 };
 
 //--------------------------------------- JOGADOR----------------------------------------------------
 
 class Personagem{
    public:
-   	sf::Texture player[3];
-   	sf::Sprite playersfml;
+   	sf::Texture TexturaJogador[3];
+   	sf::Sprite spriteJogador;
    	    int textura_corrente = 0;
-   	    int f = 0;
+   	    int contador = 0;
 
    	    Personagem() {
-   	        player[0].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(0, 0, 16, 48));
-   	        player[1].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(16, 0, 32, 48));
-   	        player[2].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(32, 0, 48, 48));
-   	        playersfml.setTexture(player[textura_corrente]);
-   	        playersfml.setPosition(130,630);
+   	    	TexturaJogador[0].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(0, 0, 16, 48));
+   	    	TexturaJogador[1].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(16, 0, 32, 48));
+   	    	TexturaJogador[2].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(32, 0, 48, 48));
+   	    	spriteJogador.setTexture(TexturaJogador[textura_corrente]);
+   	    	spriteJogador.setPosition(130,630);
    	        //Aumenta o tamanho do player
-   	        playersfml.setScale(3,3);
+   	    	spriteJogador.setScale(3,3);
    	    }
 
    	 void updatePlayer() {
-   		            f++;
-   		            if (f >= 10) {
-   		                f = 0;
+   		            contador++;
+   		            if (contador >= 10) {
+   		                contador = 0;
    		                textura_corrente = (textura_corrente + 1) % 3;
-   		                playersfml.setTexture(player[textura_corrente]);
+   		             spriteJogador.setTexture(TexturaJogador[textura_corrente]);
    		            }
    		        }
 
    	 void mover (){
-   		 sf::Vector2f position = playersfml.getPosition();
+   		 sf::Vector2f posicao = spriteJogador.getPosition();
 
    		 if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
-   			 playersfml.setPosition(position + sf::Vector2f(1 ,0));
+   			spriteJogador.setPosition(posicao + sf::Vector2f(1 ,0));
    		 }
    		 if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-   		   			 playersfml.setPosition(position + sf::Vector2f(-1 ,0));
+   			spriteJogador.setPosition(posicao + sf::Vector2f(-1 ,0));
 
    		 }
    		 if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
-   		   			 playersfml.setPosition(position + sf::Vector2f(0, -1));
+   			spriteJogador.setPosition(posicao + sf::Vector2f(0, -1));
    		  }
    		 if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-   		   		   	 playersfml.setPosition(position + sf::Vector2f(0, 1));
+   			spriteJogador.setPosition(posicao + sf::Vector2f(0, 1));
    		  }
    	 }
  };
