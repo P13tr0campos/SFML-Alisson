b7b529e7160833602948a9a301f3c7d221f10cf8
vitorialobato <136477023+vitorialobato@users.noreply.github.com>
2024-09-02 23:04:21 -0300

    Update entidades.hpp

--- Trabalho_KK_SFML/src/entidades.hpp
+++ Trabalho_KK_SFML/src/entidades.hpp
@@ -10,17 +10,6 @@
 #include <vector>
 #include <SFML/Graphics/Rect.hpp>
 #include "includes/math.hpp"
-#include "map.hpp"
-#include "Bomb.hpp"
-#include "entidades.hpp"
-
-enum ID{
-	vazio =0,
-	player,
-	projeteis,
-	inimigo,
-	plataformas
-};
 
 class Fundo {
 public:
@@ -47,48 +36,100 @@ public:
     sf::Sprite spriteBomba;
     int textura_corrente = 0;
     int f = 0;
+    // Define a velocidade de movimento
+      float speed = 200.0f;
+      bool movingRight = true;
+      sf::Clock movementClock;
 
     Bomba() {
         texturaBomba[0].loadFromFile("bombC.png", sf::IntRect(0, 0, 32, 96));
         texturaBomba[1].loadFromFile("bombC.png", sf::IntRect(32, 0, 64, 96));
         texturaBomba[2].loadFromFile("bombC.png", sf::IntRect(64, 0, 96, 96));
         spriteBomba.setTexture(texturaBomba[textura_corrente]);
+
+        spriteBomba.setPosition(130, 130);
     }
 
+
     void update() {
-        f++;
-        if (f >= 10) {
-            f = 0;
-            textura_corrente = (textura_corrente + 1) % 3;
-            spriteBomba.setTexture(texturaBomba[textura_corrente]);
+            f++;
+            if (f >= 10) {
+                f = 0;
+                // Muda para a próxima textura da animação
+                textura_corrente = (textura_corrente + 1) % 3;
+                spriteBomba.setTexture(texturaBomba[textura_corrente]);
+            }
+        }
+
+
+int movimentacao(sf::RenderWindow& window){
+    sf::Time deltaTime = movementClock.restart();
+
+    if (movingRight) {
+        spriteBomba.move(speed * deltaTime.asSeconds(), 0);
+        if (spriteBomba.getPosition().x + spriteBomba.getGlobalBounds().width >= window.getSize().x) {
+            movingRight = false;
+        }
+    } else {
+        spriteBomba.move(-speed * deltaTime.asSeconds(), 0);
+        if (spriteBomba.getPosition().x <= 0) {
+            movingRight = true;
         }
     }
+
+    return 0;
+}
 
     void draw(sf::RenderWindow& window) {
         window.draw(spriteBomba);
     }
 };
 
+//--------------------------------------- JOGADOR----------------------------------------------------
 
-/*
-class entidade{
-protected:
-	ID id;
+class Personagem{
+   public:
+   	sf::Texture player[3];
+   	sf::Sprite playersfml;
+   	    int textura_corrente = 0;
+   	    int f = 0;
 
-public:
-	entidade(math::coordenada<float> position = math::coordenada(0.f, 0.f),math::coordenada<float> tamnaho = math::coordenada(0.f, 0.f), ID id = vazio);
+   	    Personagem() {
+   	        player[0].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(0, 0, 16, 48));
+   	        player[1].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(16, 0, 32, 48));
+   	        player[2].loadFromFile("player-sfml-TP_Andando.png", sf::IntRect(32, 0, 48, 48));
+   	        playersfml.setTexture(player[textura_corrente]);
+   	        playersfml.setPosition(130,630);
+   	        //Aumenta o tamanho do player
+   	        playersfml.setScale(3,3);
+   	    }
 
-	~entidade();
+   	 void updatePlayer() {
+   		            f++;
+   		            if (f >= 10) {
+   		                f = 0;
+   		                textura_corrente = (textura_corrente + 1) % 3;
+   		                playersfml.setTexture(player[textura_corrente]);
+   		            }
+   		        }
 
-	void setaPosicao(math::coordenada pos);
+   	 void mover (){
+   		 sf::Vector2f position = playersfml.getPosition();
 
-	math::coordenada pegaPosicao() const;
+   		 if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+   			 playersfml.setPosition(position + sf::Vector2f(1 ,0));
+   		 }
+   		 if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+   		   			 playersfml.setPosition(position + sf::Vector2f(-1 ,0));
 
-	ID getId() const;
-
-
-};
-
-*/
+   		 }
+   		 if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+   		   			 playersfml.setPosition(position + sf::Vector2f(0, -1));
+   		  }
+   		 if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+   		   		   	 playersfml.setPosition(position + sf::Vector2f(0, 1));
+   		  }
+   	 }
+ };
 
 #endif /* ENTIDADES_HPP_ */
