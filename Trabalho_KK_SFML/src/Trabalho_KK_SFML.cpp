ed9763a16864390f156c84de7ebd1090d64e66e0
vitorialobato <136477023+vitorialobato@users.noreply.github.com>
2024-10-03 09:46:03 -0300

    Update Trabalho_KK_SFML.cpp

--- Trabalho_KK_SFML/src/Trabalho_KK_SFML.cpp
+++ Trabalho_KK_SFML/src/Trabalho_KK_SFML.cpp
@@ -8,41 +8,66 @@
 #include <SFML/Graphics/Rect.hpp>
 #include "includes/math.hpp"
 #include "entidades.hpp"
+const int h = 25, l = 33;
+sf::String tilemap[h] =
+			  { "                                 ",
+				"                                 ",
+				"                                 ",
+				"   BBBBBBBBBBBBBBBBBBBBBBBBBBB   ",
+				"   B           OOO           B   ",
+				"  B            OOO            B  ",
+				"  B OO                     OO B  ",
+				"  B OO                     OO B  ",
+				" B             OOO             B ",
+				" B             OOO             B ",
+				" B             OOO             B ",
+				" BOO                         OOB ",
+				" BOO                         OOB ",
+				" B             OOO             B ",
+				" B             OOO             B ",
+				" B             OOO             B ",
+				" BOO                         OOB ",
+				" BOO                         OOB ",
+				" B             OOO             B ",
+				" BOO           OOO             B ",
+				" BOO           OOO           OOB ",
+				" BOOsssssssssssssssssssssssssOOB ",
+				" BOO                         OOB ",
+				" BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB ",
+				"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB" };
 
- const int h=25, l=33;
-                    	sf::String tilemap[h] = {
-                    	"                                 ",
-                    	"                                 ",
-                    	"                                 ",
-                    	"   BBBBBBBBBBBBBBBBBBBBBBBBBBB   ",
-                    	"   B           OOO           B   ",
-                    	"  B            OOO            B  ",
-                    	"  B OO                     OO B  ",
-                    	"  B OO                     OO B  ",
-                    	" B             OOO             B ",
-                    	" B             OOO             B ",
-                    	" B             OOO             B ",
-                    	" BOO                         OOB ",
-                    	" BOO                         OOB ",
-                    	" B             OOO             B ",
-                    	" B             OOO             B ",
-                    	" B             OOO             B ",
-                    	" BOO                         OOB ",
-                    	" BOO                         OOB ",
-                    	" B             OOO             B ",
-                    	" BOO           OOO             B ",
-                    	" BOO           OOO           OOB ",
-                    	" BOO                         OOB ",
-                    	" BOO                         OOB ",
-                    	" BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB ",
-                    	"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
-                    	};
+std::vector<sf::RectangleShape> retangles() {
 
-int main(){
+	std::vector<sf::RectangleShape> retangles;
+	sf::RectangleShape rectangle(sf::Vector2f(30, 30));
+	for (unsigned int i = 0; i < h; ++i) {
+		for (int j { }; j < l; ++j) {
+			rectangle.setSize(sf::Vector2f(30, 30));
+			if (tilemap[i][j] == 'B') {
+				rectangle.setFillColor(sf::Color::Transparent);
+			}
+			if (tilemap[i][j] == 'O') {
+				rectangle.setFillColor(sf::Color::Transparent);
+			}
+			if (tilemap[i][j] == 's') {
+				rectangle.setSize(sf::Vector2f(30, 10));
+				rectangle.setFillColor(sf::Color::Magenta);
+			}
+			if (tilemap[i][j] == ' ') {
+				continue;
+			}
+			rectangle.setPosition(j * 30, i * 30);
+			retangles.push_back(rectangle);
+		}
 
+	}
 
+	return retangles;
+}
+
+int main() {
 	// Criação da janela
-	sf::RenderWindow window{ sf::VideoMode(990,750), "King Kong" };
+	sf::RenderWindow window { sf::VideoMode(990, 750), "King Kong" };
 	window.setVerticalSyncEnabled(true);
 
 	//Declaração de objetos das classes
@@ -51,51 +76,46 @@ int main(){
 	Personagem player;
 	Princesa princesa;
 	FonteTexto tempo, fase;
-    sf::Clock clock;
+	sf::Clock clock;
+	std::vector<sf::RectangleShape> rectangles = retangles();
+
 	bool temporizador = false; // Variável para controlar se o temporizador começou
 
-
 	//Loop da janela
-        while (window.isOpen())
-        {
-                sf::Event windowEvent;
-                while (window.pollEvent(windowEvent))
-                {
-                        if (windowEvent.type == sf::Event::Closed)
-                                window.close();
-                }
+	while (window.isOpen()) {
+		sf::Event windowEvent;
+		while (window.pollEvent(windowEvent)) {
+			if (windowEvent.type == sf::Event::Closed)
+				window.close();
+		}
 
-     // Atualizações
-           bomba.colisaoBomba(window);
-           princesa.colisaoPrincesa(window);
-           player.updatePlayer();
-           player.mover();
-           player.colisao(tilemap, h, l);
-           sf::RectangleShape rectangle(sf::Vector2f(30, 30));
+		// Atualizações
 
-     //Desenhar
-           window.clear(sf::Color::White);
-           window.draw(fundo.spriteFundo);
-           for (int i{}; i < h; ++i){
-        	   for (int j{}; j < l; ++j){
-                   if( tilemap[i][j] == 'B'){
-                   rectangle.setFillColor(sf::Color::Transparent);
-                   }
-                   if( tilemap[i][j] == 'O'){
-                   rectangle.setFillColor(sf::Color::Transparent);
-                   }
-                   if( tilemap[i][j] == ' '){
-                   continue;
-                   }
-                   rectangle.setPosition( j * 30, i * 30);
-                   window.draw(rectangle);
-        	   	   }
-            }
-           window.draw(bomba.spriteBomba);
-           window.draw(princesa.spritePrincesa);
-           window.draw(player.spriteJogador);
-           tempo.atualizaContador(window, temporizador);
-           fase.fasePersonagem(window);
-           window.display();
-        }
+		bomba.colisaoBomba(window);
+		princesa.colisaoPrincesa(window);
+		player.updatePlayer();
+		player.mover();
+		player.colisao(tilemap, h, l);
+
+
+		//Desenhar
+		window.clear(sf::Color::White);
+		window.draw(fundo.spriteFundo);
+
+		for (unsigned int i = 0; i < rectangles.size(); ++i) {
+			if (rectangles.at(i).getGlobalBounds().intersects(player.spriteJogador.getGlobalBounds()) and rectangles.at(i).getSize() == sf::Vector2f(30, 10)) {
+				player.spriteJogador.move(0, -player.gravidade);
+				player.ColisaoChao = true;
+			}
+
+			window.draw(rectangles.at(i));
+		}
+
+		window.draw(bomba.spriteBomba);
+		window.draw(princesa.spritePrincesa);
+		window.draw(player.spriteJogador);
+		tempo.atualizaContador(window, temporizador);
+		fase.fasePersonagem(window);
+		window.display();
+	}
 }
