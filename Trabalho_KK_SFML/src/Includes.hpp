ef59e54516a5667dc55cc68c02d26ee412e9e337
vitorialobato <136477023+vitorialobato@users.noreply.github.com>
2024-10-03 15:55:03 -0300

    Add files via upload

---
+++ Trabalho_KK_SFML/src/Includes.hpp
@@ -0,0 +1,16 @@
+#ifndef INCLUDES_HPP_
+#define INCLUDES_HPP_
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <vector>
+#include <SFML/Graphics/Rect.hpp>
+
+
+
+#endif /* INCLUDES_HPP_ */
